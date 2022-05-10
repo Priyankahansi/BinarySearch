0d5eb2710da3ff2d42e2b59178b9b1b87f94344f
Priyanka <priyankahansi95@gmail.com>
2022-05-10 21:43:29 +0530

    added:BinarySearch

---
+++ src/javashowcase/MyBinaryTreeTest.java
@@ -0,0 +1,15 @@
+package javashowcase;
+
+import com.sun.source.tree.BinaryTree;
+
+public class MyBinaryTreeTest
+{
+    public static void main(String[] args) {
+        BinarySearch<Integer> BinarySearch=new BinarySearch<>();
+        BinarySearch.add(56);
+        BinarySearch.add(30);
+        BinarySearch.add(70);
+    }
+        }
+
+
